6855c3df0ddfbc5779701aeb933fd8796d2a0ccf
himydvv <himyadav8989@gmail.com>
2026-03-13 23:49:55 +0530

    updated cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,26 @@
-const allowedOrigins = require('./allowedOrigins');
+const allowedOrigins = require('./allowedOrigins')
 
-const corsOptions ={
-    origin : (origin, callback) =>{
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin /* !origin is for tools like postman and other which do not send origin header while browesrs always send origin headers */){ 
-            callback(null, true);
+const allowedOriginsSet = new Set(allowedOrigins)
+
+const corsOptions = {
+    origin: (origin, callback) => {
+
+        // Allow requests with no origin (Postman, curl, mobile apps)
+        if (!origin) {
+            return callback(null, true)
         }
-        else{
-            callback(new Error("Not allowed by CORS"));
+
+        if (allowedOriginsSet.has(origin)) {
+            return callback(null, true)
         }
+
+        console.warn(`CORS blocked request from origin: ${origin}`)
+        return callback(new Error("Not allowed by CORS"))
     },
-    Credential:true,
-    optionsSuccessStatus:200
-};
-  module.exports = corsOptions;
+
+    credentials: true, // allow cookies / auth headers
+
+    optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions
